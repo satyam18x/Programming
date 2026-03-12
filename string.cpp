811feb973e108c8ca1f6e5559e276cac6109459f
satyam haldkar <satyamhaldkar45@gmail.com>
2026-03-12 09:37:29 +0530

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -6,7 +6,10 @@ int main(){
     string s1;
     string s2;
 
-    cin>>s1 >> s2;
+    cout << "write a string: ";
+    cin >> s1;
+    cout << "write another string: ";
+    cin >> s2;
 
-    cout<< s1 <<" " << s2 <<endl;
+    cout<<s1 <<" "<<s2<<endl;
 }
