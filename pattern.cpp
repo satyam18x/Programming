777d5167b3e80cd017fbb8b0d3344dab843ea9b4
unknown <satyamhaldkar45@gmail.com>
2026-03-15 23:26:46 +0530

    update pattern

--- pattern.cpp
+++ pattern.cpp
@@ -43,7 +43,7 @@ void print_pattern5( int n){
 
     for (int i=0;i<n;i++){
         for (int j=1; j<=n-i+1;j++){
-            cout << " * ";
+            cout << "*";
         }
         cout << endl;
     }
@@ -63,16 +63,57 @@ void print_pattern6( int n){
         cout << endl;
     }
 }
+void print_pattern7( int n){
 
+    for (int i=0;i<n;i++){
+        for (int j=0; j<i;j++){
+            cout << " ";
+        }
+         for (int j=0; j<2*n-(2*i+1);j++){
+            cout << "*";
+        }
+         for (int j=0; j<i;j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print_pattern8(int n){
+    for (int i=1;i<=2*n-1;i++){
+        int stars=i;
+        if(i>n)  stars=2*n-i;
+        for (int j=1;j<=stars;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void print_pattern9(int n){
+    int start;
+    for (int i=0;i<n;i++){
+          if(i%2==0) start=1;
+          else start=0;
+         for (int j=0;j<=i;j++){
+
+            cout << start << " ";
+            start=1-start;
+
+         }
+         cout << endl;
+    }
+}
 int main(){
            int n;
            cout << "Enter the number of rows and columns: ";
           cin >> n;
-          print_pattern1(n);
-          print_pattern2(n);
-          print_pattern3(n);
-          print_pattern4(n);
-          print_pattern5(n);
-          print_pattern6(n);
+        //   print_pattern1(n);
+        //   print_pattern2(n);
+        //   print_pattern3(n);
+        //   print_pattern4(n);
+        //   print_pattern5(n);
+        //   print_pattern6(n);
+        //   print_pattern7(n);
+        //   print_pattern8(n);
+             print_pattern9(n);
     return 0;
 }
