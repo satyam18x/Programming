cc36b5aefe2a8070c378d0bd8740174b410a9afc
satyam18x <satyamhaldkar45@gmail.com>
2026-03-21 23:04:05 +0530

    21-03-2026

---
+++ DS/Recursion/palindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool f(int i, string &s)
+{
+    if (i >= s.size() / 2)
+        return true;
+
+    if (s[i] != s[s.size() - i - 1])
+        return false;
+
+    return (i + 1, &s);
+}
+
+int main()
+{
+    string s;
+    cout << "Enter a String : ";
+    cin >> s;
+
+    cout << f(0, s);
+
+    return 0;
+}
