7b6d5244bdeb5860d57bb6d7bf0c3eb20943099d
satyam18x <satyamhaldkar45@gmail.com>
2026-03-22 20:04:17 +0530

    22-03-2026

---
+++ DS/Hashing/char_frequency.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cout << "Enter a string : ";
+    cin >> s;
+
+    // precompute
+    int hash[256] = {0};
+    for (int i = 0; i < s.size(); i++)
+        hash[s[i]] += 1;
+
+    // query
+    int q;
+    cout << "Enter how many characters you want frequency of : ";
+    cin >> q;
+
+    cout << "Enter the characters you want frequency of : ";
+    while (q--)
+    {
+
+        char c;
+        cin >> c;
+
+        // fetch
+        cout << hash[c] << endl;
+    }
+}
