88e5cd52ec96406ce772002f7cbad6bfb6f5f27f
satyam18x <satyamhaldkar45@gmail.com>
2026-04-07 22:52:03 +0530

    07-04-2026

--- DS/array/missing_number.cpp
+++ DS/array/missing_number.cpp
@@ -1,4 +1,6 @@
-//this is the better approach for finding a missing number in an array which have distint values from a range between 0 to n
+//these are the better and optimal approach for finding a missing number in an array which have distint values from a range between 0 to n
+
+//better :-
 
 // class Solution {
 // public:
@@ -15,5 +17,25 @@
 //             if(hash[i]==0)
 //             return i;
         
+//     }
+// };
+
+
+
+//optimal :-
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+
+//         int n=nums.size();
+
+//         int sum = (n*(n+1))/2;
+//         int sum2 = 0;
+
+//         for (int i = 0;i<n;i++){
+//             sum2 += nums[i];
+//         }
+//         return sum-sum2;
 //     }
 // };
