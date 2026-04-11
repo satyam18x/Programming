24eb83c31cbd3957734f0101276266f2681d9c26
satyam18x <satyamhaldkar45@gmail.com>
2026-04-11 12:21:59 +0530

    11-04-2026

--- DS/array/single_number.cpp
+++ DS/array/single_number.cpp
@@ -1,12 +1,48 @@
-//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
-//optimal solution
-// class Solution {
-// public:
-//     int singleNumber(vector<int>& nums) {
-         
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int singlenumber(vector<int> &arr)
+{
+    int n = arr.size();
+
+    int xorr = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        xorr = xorr ^ arr[i];
+    }
+    return xorr;
+}
+
+int main()
+{
+    int n;
+    cout << "Write size of array : ";
+    cin >> n;
+
+    cout << "Write an array : ";
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int result = singlenumber(arr);
+
+    cout << "single number is  : " << result;
+}
+
+// optimal solution
+//  class Solution {
+//  public:
+//      int singleNumber(vector<int>& nums) {
+
 //          int n=nums.size();
-         
+
 //          int xorr = 0;
 //          for(int i=0;i<n;i++){
 
@@ -15,30 +51,21 @@
 //          return xorr;
 
 //     }
-      
+
 // };
 
+// Brute force approach
+//  class Solution {
+//  public:
+//      int singleNumber(vector<int>& nums) {
 
-
-
-
-
-
-
-
-
-//Brute force approach
-// class Solution {
-// public:
-//     int singleNumber(vector<int>& nums) {
-         
 //          int n=nums.size();
 
 //          int num=0;
-        
+
 //          for(int i=0;i<n;i++){
 //             num = nums[i];
-           
+
 //             int count=0;
 //            for(int j=0;j<n;j++){
 //             if(nums[j]==num)
@@ -49,5 +76,5 @@
 //          }
 //         return -1;
 //     }
-      
+
 // };
