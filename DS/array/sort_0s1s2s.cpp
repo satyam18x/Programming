c0da5dc12c93b34879dc8cfeadef0285959b10e9
satyam18x <satyamhaldkar45@gmail.com>
2026-04-14 22:50:26 +0530

    14-04-2026

--- DS/array/sort_0s1s2s.cpp
+++ DS/array/sort_0s1s2s.cpp
@@ -34,3 +34,22 @@ using namespace std;
 
 
 //Better approach - Dutch National Flag Algorithm. solves in O(n)
+void sortColors(int arr[], int n) {
+    int low = 0, mid = 0, high = n - 1;
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else { // arr[mid] == 2
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+
