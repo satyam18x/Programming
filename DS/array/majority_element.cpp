762d4eb3d5fe50a784e21846759c9eebe3fff3c2
satyam18x <satyamhaldkar45@gmail.com>
2026-04-16 09:28:00 +0530

    16-04-2026

--- DS/array/majority_element.cpp
+++ DS/array/majority_element.cpp
@@ -1,26 +1,48 @@
 //Brute force
 
-// class Solution {
-// public:
-//     int majorityElement(vector<int>& nums) {
-//         int n=nums.size();
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
 
         
-//         for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
 
-//              int count=0;
-//              for(int j=0;j<n;j++){
-//                 if(nums[i]==nums[j]){
-//                     count++;
-//                 }
+             int count=0;
+             for(int j=0;j<n;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
 
-//                 if(count>n/2)
-//                 return nums[i];
-//              }
+                if(count>n/2)
+                return nums[i];
+             }
 
             
-//         }
-//         return -1;
-//     }
-// };
+        }
+        return -1;
+    }
+};
+
+//Better approach
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+
+        map <int,int> mpp;
+        
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+
+        }
+
+        for(auto it : mpp){
+            if(it.second>n/2){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
 
