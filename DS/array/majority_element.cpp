0ab17f903e8c708085c29c7b16f898b4b971ff4b
satyam18x <satyamhaldkar45@gmail.com>
2026-04-16 10:04:27 +0530

    16-04-2026

--- DS/array/majority_element.cpp
+++ DS/array/majority_element.cpp
@@ -1,48 +1,82 @@
 //Brute force
 
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int n=nums.size();
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n=nums.size();
 
         
-        for(int i=0;i<n;i++){
+//         for(int i=0;i<n;i++){
 
-             int count=0;
-             for(int j=0;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
+//              int count=0;
+//              for(int j=0;j<n;j++){
+//                 if(nums[i]==nums[j]){
+//                     count++;
+//                 }
 
-                if(count>n/2)
-                return nums[i];
-             }
+//                 if(count>n/2)
+//                 return nums[i];
+//              }
 
             
-        }
-        return -1;
-    }
-};
+//         }
+//         return -1;
+//     }
+// };
 
 //Better approach
-class Solution {
-public:
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n=nums.size();
+
+//         map <int,int> mpp;
+        
+//         for(int i=0;i<n;i++){
+//             mpp[nums[i]]++;
+
+//         }
+
+//         for(auto it : mpp){
+//             if(it.second>n/2){
+//                 return it.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+//optimal 
+#include <iostream>
+#include <vector>
+using namespace std;
+
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
 
-        map <int,int> mpp;
+        int count=0;
+        int el;
         
         for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
-
+         if(count==0){
+            count++;
+            el=nums[i];
+         }
+         else if(el==nums[i]){
+            count++;
+         }
+         else{count--;
+         }
         }
 
-        for(auto it : mpp){
-            if(it.second>n/2){
-                return it.first;
-            }
-        }
-        return -1;
+        int count1=0;
+        for(int i=0;i<n;i++){
+          if(nums[i]==el){
+            count1++;
+          }
+        if(count1>n/2)
+        return el;
+        }       
+         return -1;
     }
-};
 
