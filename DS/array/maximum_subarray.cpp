7e134a210633148472dac03f5a80153e8a5d2995
satyam18x <satyamhaldkar45@gmail.com>
2026-04-17 11:50:25 +0530

    17-04-2026

--- DS/array/maximum_subarray.cpp
+++ DS/array/maximum_subarray.cpp
@@ -24,3 +24,26 @@ public:
 
 //better approach  - TC-> O(N^2)
 
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int n=nums.size();
+
+        int maxi=INT_MIN;
+
+        for(int i=0;i<n;i++){
+             int sum=0;
+            for(int j=i;j<n;j++)
+               
+                {
+                    sum+=nums[j];
+                     maxi=max(sum , maxi);
+                }
+                 
+             
+        }
+         return maxi;
+        
+    }
+};
