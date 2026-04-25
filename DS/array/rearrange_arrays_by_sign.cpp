044cf9672b4e1842d53dc8e319e137a461bd8488
satyam18x <satyamhaldkar45@gmail.com>
2026-04-25 11:12:27 +0530

    25-04-26

--- DS/array/rearrange_arrays_by_sign.cpp
+++ DS/array/rearrange_arrays_by_sign.cpp
@@ -24,3 +24,27 @@ public:
 
 //more better than brute force 
 
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        
+        int n=nums.size();
+        vector <int> ans(n);
+        int posIdx=0, NegIdx=1;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>=0){
+                ans[posIdx]=nums[i];
+                posIdx+=2;
+
+            }
+            else
+            {
+                ans[NegIdx]=nums[i];
+                NegIdx+=2;
+            }
+        }
+        return ans;
+    }
+
+};
