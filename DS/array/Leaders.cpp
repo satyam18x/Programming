a59bd8563924abef90dd7c6ab57fe65ede9b2702
satyam18x <satyamhaldkar45@gmail.com>
2026-04-27 23:31:40 +0530

    27-04-26

---
+++ DS/array/Leaders.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> leaders(vector<int>& nums) {
+      
+      int n=nums.size();
+      int maxi=INT_MIN;
+
+      vector <int> ans;
+
+      for(int i=n-1;i>=0;i--){
+        if(nums[i]>maxi){
+            ans.push_back(nums[i]);
+        }
+        maxi = max(nums[i],maxi);
+      }
+      reverse(ans.begin(),ans.end());
+      return ans;
+    }
+
+};
