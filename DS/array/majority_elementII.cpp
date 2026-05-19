2b6900e0217615a345556ee4a8060a8644f23343
satyam18x <satyamhaldkar45@gmail.com>
2026-05-19 19:55:23 +0530

    19-05-2026

--- DS/array/majority_elementII.cpp
+++ DS/array/majority_elementII.cpp
@@ -57,4 +57,23 @@ public:
 //we can reduce complexity by removing two iteration to only one iteration which will be better solution no. 2 
 //Tc - O(n)+O(n log n)
 
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
 
+        int n=nums.size();
+        vector <int> ans;
+       int mini=int ((n/3)+1);
+        map <int,int> mpp;
+
+        for(int i=0;i<n;i++){
+             mpp[nums[i]]++;
+             
+             if(mpp[nums[i]]==mini){
+                
+                ans.push_back(nums[i]);
+             }
+        }
+        return ans;
+    }
+};
