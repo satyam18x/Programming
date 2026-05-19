b6817b46a577933d060aea89f22e0e921797de21
satyam18x <satyamhaldkar45@gmail.com>
2026-05-19 19:54:34 +0530

    19-05-2026

--- DS/array/majority_elementII.cpp
+++ DS/array/majority_elementII.cpp
@@ -29,3 +29,32 @@ public:
         return ans;
     }
 };
+
+//Better solution no.1 = TC- O(N^2) + o(n log n)
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+
+        int n=nums.size();
+        vector <int> ans;
+       
+        map <int,int> mpp;
+
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+
+
+        for(auto it : mpp){
+            if(it.second>n/3)
+                ans.push_back(it.first);
+            
+        }
+        return ans;
+    }
+};
+
+//we can reduce complexity by removing two iteration to only one iteration which will be better solution no. 2 
+//Tc - O(n)+O(n log n)
+
+
