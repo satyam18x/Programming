d27c75ae01771f10bc137687b77b4011978c563e
satyam18x <satyamhaldkar45@gmail.com>
2026-05-20 22:11:47 +0530

    20-05-2026

--- DS/array/3sum.cpp
+++ DS/array/3sum.cpp
@@ -1,1 +1,29 @@
 //brute force
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+         int n=nums.size();
+      
+       set <vector <int>> st;                      //using set data structure for storing unique triplets only
+       
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                  int  sum=nums[i]+nums[j]+nums[k];
+
+                    if(sum==0)  //checking if sum is zero
+                    {       
+
+                        vector <int> temp = {nums[i],nums[j],nums[k]};        //checking if the triplets are unique or not, so first sort them and storing them in set data structure which will store only unique triplets
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+            
+        }
+         vector < vector<int>> ans (st.begin(),st.end()) ;
+         return ans;
+    }
+    
+};
