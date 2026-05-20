215ddf33ba7c2873480ee7dac0eb62f6cf46e278
satyam18x <satyamhaldkar45@gmail.com>
2026-05-20 23:21:14 +0530

    20-05-2026

--- DS/array/3sum.cpp
+++ DS/array/3sum.cpp
@@ -54,4 +54,41 @@ public:
     
 };
 
-//optimal solution
+//optimal solution     -- Tc will be O (N^2)+ n log n  and SC - o(no. of unique triplets)
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+         int n=nums.size();
+
+         vector <vector <int>> ans;
+          
+         sort(nums.begin(),nums.end());
+
+         for(int i=0;i<n;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+
+                if(sum<0){
+                    j++;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else {
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+         } 
+         
+      return ans;
+    }
+    
+};
