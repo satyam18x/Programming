981989a0df3feb95dfe12e1bf3ff20596b7a6fcd
satyam18x <satyamhaldkar45@gmail.com>
2026-05-21 11:14:45 +0530

    21-05-2026

--- DS/array/4 sum.cpp
+++ DS/array/4 sum.cpp
@@ -1,1 +1,32 @@
 //brute force - Tc-O(n^4)
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+        int n=nums.size();
+
+        set <vector <int>> st;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+              for(int k=j+1;k<n;k++){
+                for(int l=k+1;l<n;l++){
+               
+                long long sum = (long long) nums[i]+nums[j]+nums[k]+nums[l];
+                
+                if (sum==target){
+                    
+                    vector <int> temp = {nums[i],nums[j],nums[k],nums[l]};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
+                }
+                }
+               }
+            }
+        } 
+        
+         vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
+
+    }
+};
