7216bb005d08edfea36fc2bd8e7c94d05302d13b
satyam18x <satyamhaldkar45@gmail.com>
2026-05-21 17:09:41 +0530

    21-05-2026

--- DS/array/mergeIntervals.cpp
+++ DS/array/mergeIntervals.cpp
@@ -1,2 +1,33 @@
 //brute force - Tc-O(n log n) +O(2n) , SC- O(n)
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
 
+        int n=intervals.size();
+       sort(intervals.begin(),intervals.end());
+        vector <vector <int>> ans;
+
+        for(int i=0;i<n;i++){
+            int start=intervals[i][0];
+            int end=intervals[i][1];
+            
+            if(!ans.empty() && end <= ans.back()[1]) continue;
+
+
+            for(int j=i+1;j<n;j++){
+                if(intervals[j][0] <= end){
+                    end = max(end,intervals[j][1]);
+                }
+                else{
+                    break;
+                }
+            }
+            ans.push_back({start,end});
+        }
+
+        return ans;
+    
+
+        
+    }
+};
