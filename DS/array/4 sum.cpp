49b591fb6e4747be360ecb9488643b1b03483a4a
satyam18x <satyamhaldkar45@gmail.com>
2026-05-21 11:36:43 +0530

    21-05-2026

--- DS/array/4 sum.cpp
+++ DS/array/4 sum.cpp
@@ -33,5 +33,38 @@ public:
 
 //Better solution - TC - O(n^3) + log (m) as using hashset  and Sc - O(n) + o(no.quads)*2
 
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+        int n=nums.size();
 
+        set <vector <int>> st;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                set <long long> hashset;
+              for(int k=j+1;k<n;k++){
+                
+               
+                long long sum = (long long)nums[i]+nums[j]+nums[k];
+                long long fourth = target - sum;
+                
+                
+                    if(hashset.find(fourth)!=hashset.end()){
+                    vector <int> temp = {nums[i],nums[j],nums[k],(int)fourth};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
+                
+                    }
+                    hashset.insert(nums[k]);
+               }
+            }
+        } 
+        
+         vector<vector<int>> ans(st.begin(),st.end());
+        return ans;
+
+    }
+};
                
