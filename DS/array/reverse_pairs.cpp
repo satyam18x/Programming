a35d30e779e6b4396eae56f5721723552d8c5c3f
satyam18x <satyamhaldkar45@gmail.com>
2026-05-22 23:04:10 +0530

    22-05-2026

--- DS/array/reverse_pairs.cpp
+++ DS/array/reverse_pairs.cpp
@@ -1,2 +1,20 @@
 //Brute force - TC-o(n^2) , SC- O(1)
 
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+
+        int n=nums.size();
+
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                 
+                 if(nums[i]>2LL * nums[j])
+                 count ++;
+            }
+        }
+        return count;
+    }
+};
