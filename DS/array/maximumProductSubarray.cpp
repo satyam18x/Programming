1e5e8c6851aec7a599b5dc5e5231d168faf43f5e
satyam18x <satyamhaldkar45@gmail.com>
2026-05-23 11:35:41 +0530

    23-05-2026

--- DS/array/maximumProductSubarray.cpp
+++ DS/array/maximumProductSubarray.cpp
@@ -1,2 +1,22 @@
 //Bruteforce - TC O(n^3)
 
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+
+        int n=nums.size();
+        int productmax =INT_MIN;
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int product=1;
+                for(int k=i;k<=j;k++){
+                    
+                    product = product*nums[k];
+                }
+                productmax = max (productmax,product);
+            }
+        }
+        return productmax;
+    }
+};
