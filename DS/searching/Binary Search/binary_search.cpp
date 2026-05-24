e1f4ea48bbdb7f0bd6b86a65dab010b1ed9dd8f8
satyam18x <satyamhaldkar45@gmail.com>
2026-05-24 23:18:58 +0530

    24-05-2026

--- DS/searching/Binary Search/binary_search.cpp
+++ DS/searching/Binary Search/binary_search.cpp
@@ -1,1 +1,27 @@
 //Iterative code 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+       
+        int n=nums.size();
+        int low = 0;
+        int upper = n - 1;
+
+        while (low <= upper) {
+
+            int mid = (low + upper) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                upper = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
