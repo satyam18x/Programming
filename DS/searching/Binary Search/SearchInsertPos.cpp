dbe144eb58c2bfd7aed72f1c4e14657ad3f9a48a
satyam18x <satyamhaldkar45@gmail.com>
2026-05-28 22:30:11 +0530

    28-05-2026

--- DS/searching/Binary Search/SearchInsertPos.cpp
+++ DS/searching/Binary Search/SearchInsertPos.cpp
@@ -1,2 +1,17 @@
 //using Linear search
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+            if(nums[i]>=target)
+            return i;
+        }
+        return n;
+        
+    }
+};
+
 
