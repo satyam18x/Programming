4db3fc32491292c9b8be54c7e4004379a0f24d42
satyam18x <satyamhaldkar45@gmail.com>
2026-05-30 11:04:38 +0530

    30-05-2026

--- DS/searching/Binary Search/first_last_occur.cpp
+++ DS/searching/Binary Search/first_last_occur.cpp
@@ -1,1 +1,24 @@
 //Using linear search O(N)
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+          int n=nums.size();
+          int first=-1;
+          int last = -1;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==target)
+            {
+                if(first==-1)
+                   first=i;
+
+
+                    last=i;
+            }
+       }
+       
+        return {first,last};
+        
+    }
+};
