5e9645ab8b1b5cc5e71c5732867f26747feeba18
satyam18x <satyamhaldkar45@gmail.com>
2026-05-30 12:08:24 +0530

    30-05-2026

--- DS/searching/Binary Search/first_last_occur.cpp
+++ DS/searching/Binary Search/first_last_occur.cpp
@@ -115,3 +115,67 @@ public:
      return first;
 }
 
+class Solution {
+public:
+       
+    int firstOccurence(vector<int>& nums, int target) {
+
+        int n=nums.size();
+
+     int low=0;
+     int high = n-1;
+     int first = -1;;
+
+     while(low<=high){
+        int mid=(low+high)/2;
+
+        if(nums[mid]==target){
+            first = mid;
+            high = mid-1;
+        }
+        else if(nums[mid]>target){
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+     }
+     return first;
+}
+
+    int lastOccurence(vector<int>& nums, int target) {
+
+        int n=nums.size();
+
+     int low=0;
+     int high = n-1;
+     int last=-1;
+
+     while(low<=high){
+        int mid=(low+high)/2;
+
+        if(nums[mid]==target){
+            last=mid;
+            low=mid+1;
+        }
+        else if(nums[mid]>target){
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+     }
+     return last;
+}    
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+        int n=nums.size();
+
+        int first= firstOccurence(nums,target);
+        int last=lastOccurence(nums,target);
+        return {first,last};
+
+       
+    }
+};
