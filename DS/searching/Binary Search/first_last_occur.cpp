53d3402849ccae03efd63939ff65b7d02d9e4ea6
satyam18x <satyamhaldkar45@gmail.com>
2026-05-30 11:55:29 +0530

    30-05-2026

--- DS/searching/Binary Search/first_last_occur.cpp
+++ DS/searching/Binary Search/first_last_occur.cpp
@@ -49,3 +49,37 @@ public:
      return ans;
 }
 
+    int upperBound(vector<int>& nums, int target) {
+
+        int n=nums.size();
+
+     int low=0;
+     int high = n-1;
+     int ans = n;
+
+     while(low<=high){
+        int mid=(low+high)/2;
+
+        if(nums[mid]>target){
+            ans=mid;
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+     }
+     return ans;
+}    
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+        int n=nums.size();
+
+        int lb=lowerBound(nums,target);
+        if(lb==n || nums[lb]!=target)
+         return {-1,-1};
+        else
+         return {lb,upperBound(nums,target)-1};  
+        
+    }
+};
