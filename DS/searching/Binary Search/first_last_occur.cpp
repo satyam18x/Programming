3fdda6f1c659ddb8e5d870b90240ce09cb0551f2
satyam18x <satyamhaldkar45@gmail.com>
2026-05-30 11:55:08 +0530

    30-05-2026

--- DS/searching/Binary Search/first_last_occur.cpp
+++ DS/searching/Binary Search/first_last_occur.cpp
@@ -24,3 +24,28 @@ public:
 };
 
 //Now using binary search
+class Solution {
+public:
+       
+    int lowerBound(vector<int>& nums, int target) {
+
+        int n=nums.size();
+
+     int low=0;
+     int high = n-1;
+     int ans = n;
+
+     while(low<=high){
+        int mid=(low+high)/2;
+
+        if(nums[mid]>=target){
+            ans=mid;
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+     }
+     return ans;
+}
+
