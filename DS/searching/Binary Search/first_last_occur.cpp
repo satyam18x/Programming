6b14965015d111ec7562c386e4164de848dd2803
satyam18x <satyamhaldkar45@gmail.com>
2026-05-30 12:08:05 +0530

    30-05-2026

--- DS/searching/Binary Search/first_last_occur.cpp
+++ DS/searching/Binary Search/first_last_occur.cpp
@@ -86,3 +86,32 @@ public:
 
 
 //Now using COmplete Binary search , without using upper and lower bound
+
+class Solution {
+public:
+       
+    int firstOccurence(vector<int>& nums, int target) {
+
+        int n=nums.size();
+
+     int low=0;
+     int high = n-1;
+     int first = -1;;
+
+     while(low<=high){
+        int mid=(low+high)/2;
+
+        if(nums[mid]==target){
+            first = mid;
+            high = mid-1;
+        }
+        else if(nums[mid]>target){
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+     }
+     return first;
+}
+
