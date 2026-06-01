3cf769bbb4030c117a778ada2fe98e52f69f7381
satyam18x <satyamhaldkar45@gmail.com>
2026-06-01 10:27:02 +0530

    01-06-2026

--- DS/searching/Binary Search/find_min_in_rotated_sorted_array.cpp
+++ DS/searching/Binary Search/find_min_in_rotated_sorted_array.cpp
@@ -1,1 +1,13 @@
 //using linear search - O(N)
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+        int n=nums.size();
+        int mini = nums[0];
+        for(int i=1;i<n;i++){
+             mini = min(mini,nums[i]);
+        }
+         return mini;
+    }
+};
