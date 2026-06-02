d7df0843b37e9f1170c86eb3688d7cc964135b4b
satyam18x <satyamhaldkar45@gmail.com>
2026-06-02 23:27:53 +0530

    02-06-2026

--- DS/searching/Binary Search/find_single_element.cpp
+++ DS/searching/Binary Search/find_single_element.cpp
@@ -34,21 +34,21 @@ public:
 
        int n=nums.size();
        
-       if(n==1) return nums[0];
-       if(nums[0]!=nums[1]) return nums[0];
-       if(nums[n-1]!=nums[n-2]) return nums[n-1];
-
+       if(n==1) return nums[0];                     //if there is only one element in array, return that elem
+       if(nums[0]!=nums[1]) return nums[0];         //if first element don't have next element equal to it ,then it is that single element , so return it
+       if(nums[n-1]!=nums[n-2]) return nums[n-1];   // same with the last eleement , if it doesnt have its  previous elem equal to it , return it
+     
        int low = 1;
        int high = n-1;
 
        while(low<=high){
         int mid=(low+high)/2;
 
-        if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
+        if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])     //if mid doesnt have previous and next element equal to it , it is the single element   
           return nums[mid];
     
 
-       if((mid%2==1 && nums[mid] == nums[mid-1]) 
+       if((mid%2==1 && nums[mid] == nums[mid-1])                  
               || (mid % 2 == 0 && nums[mid]==nums[mid+1]))
           {
                low=mid+1;
