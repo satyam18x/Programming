b445d679b7c05a03f6e19ac79a68c8e3592aceed
satyam18x <satyamhaldkar45@gmail.com>
2026-06-05 11:11:21 +0530

    05-06-2026

--- DS/searching/Binary Search/sqrt_using_BS.cpp
+++ DS/searching/Binary Search/sqrt_using_BS.cpp
@@ -17,3 +17,26 @@ public:
 };
 
 //using binary search
+class Solution {
+public:
+    int floorSqrt(int n)  {
+      
+       int low=1 , high =n;
+       int ans=1;
+
+       while(low<=high){
+        long long mid=(low+high)/2;
+        long long val=mid*mid;
+
+        if(val<=n){
+            ans=mid;
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+       }
+       return ans;
+     
+    }
+};
