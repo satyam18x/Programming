3e592959d580326a47b2aea9501b4f8c6f6dc5ca
satyam18x <satyamhaldkar45@gmail.com>
2026-06-05 10:47:44 +0530

    05-06-2026

--- DS/searching/Binary Search/sqrt_using_BS.cpp
+++ DS/searching/Binary Search/sqrt_using_BS.cpp
@@ -1,1 +1,17 @@
 //using linear search
+class Solution {
+public:
+    int floorSqrt(int n)  {
+      
+       int ans=1;
+       for(int i=1;i<=n;i++){
+        
+        if(i*i<=n){
+            ans=i;
+        }
+        else 
+            break;
+       }
+       return ans;
+    }
+};
