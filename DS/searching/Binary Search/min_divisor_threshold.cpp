d49b967ae069144957ee42c51594754d02f2572a
satyam18x <satyamhaldkar45@gmail.com>
2026-06-13 23:42:20 +0530

    13-06-2026

--- DS/searching/Binary Search/min_divisor_threshold.cpp
+++ DS/searching/Binary Search/min_divisor_threshold.cpp
@@ -1,4 +1,25 @@
 //Brute force
+class Solution {
+public:
+
+    int smallestDivisor(vector<int>& nums, int threshold){
+      
+        int maxi = *max_element(nums.begin(),nums.end());                               
+       
+        int n=nums.size();
+
+        for(int div=1;div<=maxi;div++){
+             int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+ceil((double)(nums[i])/div);
+        }
+          if(sum<=threshold)
+       return div;
+        }
+        return -1;
+    } 
+  
+};
 
 //optimal using binary search
 class Solution {
