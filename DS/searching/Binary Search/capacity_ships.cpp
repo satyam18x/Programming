2afac7c7c890b14ae5e1f4bd479f2a66a8aeacc6
satyam18x <satyamhaldkar45@gmail.com>
2026-06-16 11:32:38 +0530

    16-06-26

--- DS/searching/Binary Search/capacity_ships.cpp
+++ DS/searching/Binary Search/capacity_ships.cpp
@@ -30,3 +30,38 @@ public:
 };
 
 //Optimal using binary search
+class Solution {
+public:
+    int daysRequired(vector<int>& weights, int cap) {
+        int days = 1;
+        int load = 0;
+
+        for (int i=0;i<weights.size();i++) {
+            if (load + weights[i] > cap) {
+                days++;
+                load = weights[i];
+            } else {
+                load += weights[i];
+            }
+        }
+        return days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+
+        while(low<=high) {
+            int mid=(low+high)/2;
+            if (daysRequired(weights, mid) <= days)
+               {
+                high = mid-1;
+               }
+            else{
+                low=mid+1;
+            }   
+        }
+
+        return low;
+    }
+};
