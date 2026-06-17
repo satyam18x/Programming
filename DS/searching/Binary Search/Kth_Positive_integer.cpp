9c3d6aa1bcffc7b35b050e9828a21e8fb048b67c
satyam18x <satyamhaldkar45@gmail.com>
2026-06-17 11:34:05 +0530

    17-06-26

--- DS/searching/Binary Search/Kth_Positive_integer.cpp
+++ DS/searching/Binary Search/Kth_Positive_integer.cpp
@@ -1,1 +1,14 @@
 //Brute Force
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]<=k)
+             k++; 
+        }
+           return k;
+
+    }
+};
