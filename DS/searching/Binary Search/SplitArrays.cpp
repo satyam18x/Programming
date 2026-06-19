a0c0369acbe31aa02fdf4ecb2aef5eeb203d2fc5
satyam18x <satyamhaldkar45@gmail.com>
2026-06-19 16:20:56 +0530

    19-06-2026

--- DS/searching/Binary Search/SplitArrays.cpp
+++ DS/searching/Binary Search/SplitArrays.cpp
@@ -1,1 +1,31 @@
 //brute force
+class Solution {
+public:
+    int CanSplit(vector<int> &arr, int maxsum){
+        int parts=1;
+        long long current=0;
+
+        for(int i=0;i<arr.size();i++){
+            if(current+arr[i]<=maxsum)
+               {
+                current+=arr[i];
+               }
+            else{
+                parts++;
+                current=arr[i];
+            }   
+        }
+        return parts;
+
+    }
+    int splitArray(vector<int> &nums, int k)  {
+        int low=*max_element(nums.begin(),nums.end());
+        int high=accumulate(nums.begin(),nums.end(),0);
+
+        for(int maxsum=low;maxsum<=high;maxsum++){
+            if(CanSplit(nums,maxsum)<=k)
+            return maxsum;
+        }
+        return -1;
+    }
+};
