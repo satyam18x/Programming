eaf2f3e7c7c92259aac07008b809e177290aa6b8
satyam18x <satyamhaldkar45@gmail.com>
2026-06-19 16:02:02 +0530

    19-06-2026

--- DS/searching/Binary Search/Allocate_books.cpp
+++ DS/searching/Binary Search/Allocate_books.cpp
@@ -32,3 +32,39 @@ public:
 
 
 //binarySearch
+class Solution {
+public:
+    int CountStudents(vector<int> &arr, int pages){
+        int stu=1;
+        long long pagesStudent=0;
+
+        for(int i=0;i<arr.size();i++){
+            if(pagesStudent+arr[i]<=pages)
+               {
+                pagesStudent+=arr[i];
+               }
+            else{
+                stu++;
+                pagesStudent=arr[i];
+            }   
+        }
+        return stu;
+
+    }
+    int findPages(vector<int> &nums, int m)  {
+        int low=*max_element(nums.begin(),nums.end());
+        int high=accumulate(nums.begin(),nums.end(),0);
+
+       while(low<=high){
+        int mid = (low+high)/2;
+            if(CountStudents(nums,mid)>m){
+               low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+            
+        }
+        return low;
+    }
+};
