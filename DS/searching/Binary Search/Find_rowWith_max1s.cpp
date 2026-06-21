32570a4312547ef5884d7729df8b5ba6c615d9c1
satyam18x <satyamhaldkar45@gmail.com>
2026-06-21 20:58:00 +0530

    21-06-2026

--- DS/searching/Binary Search/Find_rowWith_max1s.cpp
+++ DS/searching/Binary Search/Find_rowWith_max1s.cpp
@@ -1,1 +1,23 @@
 //brute force
+class Solution {
+  public:   
+  int rowWithMax1s(vector < vector < int >> & mat) {
+       int n=mat.size();
+       int maxcount=0;
+       int ans=-1;
+
+         for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=i;j<n;j++){
+                   if(mat[i][j]==1)
+                   count++;
+            }
+            if(count>maxcount){
+            maxcount=count;
+            ans=i;
+         }
+         }
+         
+            return ans;
+  }
+};
