6857439e0dea7aed63b3a9fa2bfb0b491b553989
satyam18x <satyamhaldkar45@gmail.com>
2026-06-21 21:43:12 +0530

    21-06-2026

--- DS/searching/Binary Search/SearchIn_2D.cpp
+++ DS/searching/Binary Search/SearchIn_2D.cpp
@@ -1,1 +1,17 @@
-//brute force
+//brute force - O(n*m)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == target)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
