91b316027a4037aeb35427fa51dbfe278f372676
satyam18x <satyamhaldkar45@gmail.com>
2026-06-26 13:19:33 +0530

    2-06-26

--- DS/Tree/Inorder.cpp
+++ DS/Tree/Inorder.cpp
@@ -18,3 +18,27 @@ public:
     }
 };
 //Iterative
+class Solution {
+public:
+   
+    vector<int> inorderTraversal(TreeNode* root) {
+       stack <TreeNode*> st;
+       TreeNode *node = root;
+        vector <int> inorder;
+       
+       while(true){
+           if(node!=NULL){
+            st.push(node);
+            node = node->left;
+           }
+           else{
+               if (st.empty()==true) break;
+               node=st.top();
+               st.pop();
+               inorder.push_back(node->val);
+               node = node->right;
+           }
+       }
+       return inorder;
+    }
+};
