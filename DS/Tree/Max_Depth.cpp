66674037f197231d45939bf565c7810d20b0da0c
satyam18x <satyamhaldkar45@gmail.com>
2026-06-27 12:41:19 +0530

    27-06-26

--- DS/Tree/Max_Depth.cpp
+++ DS/Tree/Max_Depth.cpp
@@ -1,1 +1,14 @@
 //MAxDepth of a binary tree
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+
+       if(root==NULL) return 0;
+
+       int lh = maxDepth(root->left);
+       int rh = maxDepth(root->right);
+
+       return 1+max(lh,rh);
+        
+    }
+};
