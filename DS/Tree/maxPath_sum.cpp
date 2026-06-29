55a99cdfbc69020c9f964131b185b6b4c1890301
satyam18x <satyamhaldkar45@gmail.com>
2026-06-29 11:30:20 +0530

    29-06-26

--- DS/Tree/maxPath_sum.cpp
+++ DS/Tree/maxPath_sum.cpp
@@ -1,1 +1,25 @@
 //Tc-O(n)
+class Solution {
+public:
+    int sum=0;
+    int maxi = INT_MIN;
+    int summ(TreeNode* root){
+     if(root==NULL) return 0;
+     
+     
+     int lh = max(0,summ(root->left));
+     int rh = max(0,summ(root->right));
+     
+     sum = lh+rh+root->val;
+     maxi = max(maxi,sum);
+
+     return max(lh,rh)+root->val;
+
+    }
+    int maxPathSum(TreeNode* root) {
+        
+        summ(root);
+        return maxi;
+
+    }
+};
