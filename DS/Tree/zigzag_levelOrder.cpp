3854d4b3103ab7be5f102800817f56d69b6d8814
satyam18x <satyamhaldkar45@gmail.com>
2026-06-30 17:49:21 +0530

    30-06-26

--- DS/Tree/zigzag_levelOrder.cpp
+++ DS/Tree/zigzag_levelOrder.cpp
@@ -1,1 +1,32 @@
 //TC -O(n)
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if(root==NULL) return result;
+
+        queue<TreeNode*> que;
+        que.push(root);
+
+        int lefttoRight = true;
+
+        while(!que.empty()){
+            int size = que.size();
+            vector<int> level(size);
+            
+            for(int i=0;i<size;i++){
+                TreeNode *node = que.front();
+                que.pop();
+
+                int index = lefttoRight ? i : size-1-i;
+                level[index] = node->val;  
+                if(node->left!=NULL) que.push(node->left);
+                if(node->right!=NULL) que.push(node->right);
+                
+            }
+            lefttoRight=!lefttoRight;
+            result.push_back(level);
+        }
+         return result;
+    }
+};
