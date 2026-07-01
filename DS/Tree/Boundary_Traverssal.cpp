a4b39f55474d95d6c4f56c92ea86f8426630826a
satyam18x <satyamhaldkar45@gmail.com>
2026-07-01 13:46:02 +0530

    01-07-26

--- DS/Tree/Boundary_Traverssal.cpp
+++ DS/Tree/Boundary_Traverssal.cpp
@@ -20,4 +20,40 @@ class Solution {
         }
        }
     }
- 
+    void addLeaves(Node* root, vector<int>& res){
+        if(isLeaf(root)){
+            res.push_back(root->data);
+            return;
+        }
+        if(root->left) addLeaves(root->left,res);
+        if(root->right) addLeaves(root->right,res);
+    }
+    void addRightboundary(Node* root, vector<int>& res){
+         Node* cur = root->right;
+         vector <int> temp;
+        
+       while(cur){ 
+        if(!isLeaf(cur)) temp.push_back(cur->data);
+        if(cur->right){
+            cur = cur->right;
+        }
+        else {
+            cur = cur->left;
+        }
+       }
+       for(int i=temp.size()-1;i>=0;i--){
+           res.push_back(temp[i]);
+       }
+    }
+    vector<int> boundaryTraversal(Node *root) {
+        
+        vector<int> res;
+        if(root==NULL) return res;
+        if(!isLeaf(root)) res.push_back(root->data);
+        addLeftboundary(root,res);
+        addLeaves(root,res);
+        addRightboundary(root,res);
+        
+        return res;
+    }
+};
