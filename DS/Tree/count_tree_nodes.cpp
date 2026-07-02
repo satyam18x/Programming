1e482c6aa969817ae44b654c94644e7188606f05
satyam18x <satyamhaldkar45@gmail.com>
2026-07-02 09:41:48 +0530

    02-07-26

--- DS/Tree/count_tree_nodes.cpp
+++ DS/Tree/count_tree_nodes.cpp
@@ -1,1 +1,19 @@
 //TC-O(n)
+class Solution {
+public:
+
+    int countt(TreeNode* root){
+        if(root==NULL) return 0;
+
+       int lh = countt(root->left);
+       int rh = countt(root->right);
+
+       return 1+lh+rh;
+    }
+
+    int countNodes(TreeNode* root) {
+      
+    return countt(root);
+    
+    }
+};
