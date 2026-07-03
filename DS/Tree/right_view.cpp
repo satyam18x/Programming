84e787dfda79c9379ee07142d8c6dae981272d2a
satyam18x <satyamhaldkar45@gmail.com>
2026-07-03 11:18:21 +0530

    03-07-26

--- DS/Tree/right_view.cpp
+++ DS/Tree/right_view.cpp
@@ -1,1 +1,17 @@
 //recursive - O(n)
+class Solution {
+public:
+   void right(TreeNode* root, int level , vector <int>& res){
+    if(root==NULL) return;
+    if(level==res.size()) res.push_back(root->val);
+    right(root->right,level+1,res);
+    right(root->left,level+1,res);
+
+   }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        right(root,0,res);
+        return res;
+
+    }
+};
