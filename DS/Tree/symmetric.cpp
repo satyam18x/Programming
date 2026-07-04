a129b14755493ce4115de9fdc14f9e598ea08c9e
satyam18x <satyamhaldkar45@gmail.com>
2026-07-04 13:01:33 +0530

    04-07-26

--- DS/Tree/symmetric.cpp
+++ DS/Tree/symmetric.cpp
@@ -1,1 +1,23 @@
 Tc-O(n) sc-O(h)
+class Solution {
+public:
+    bool isMirror(TreeNode* left, TreeNode* right){
+        if(left==NULL && right==NULL)
+        return true;
+
+        if(left==NULL || right==NULL)
+        return false;
+
+        if(left->val != right->val)
+        return false;
+
+        return isMirror(left->left,right->right) && isMirror(left->right,right->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        
+        if(root==NULL) return false;
+        return isMirror(root->left,root->right);
+
+    }
+};
