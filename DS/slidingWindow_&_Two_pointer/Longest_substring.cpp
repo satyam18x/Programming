ae62fe0d507c2dd695334dc9e6ae59501efdd3c9
satyam18x <satyamhaldkar45@gmail.com>
2026-07-06 11:46:58 +0530

    06-07-26

--- DS/slidingWindow_&_Two_pointer/Longest_substring.cpp
+++ DS/slidingWindow_&_Two_pointer/Longest_substring.cpp
@@ -1,1 +1,20 @@
 //Longest substring without duplicate chaarcters
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+      
+      int n=s.size();
+      int maxlen = 0;
+
+      for(int i=0;i<n;i++){
+                 int hash[256] = {0};
+        for(int j=i;j<n;j++){
+              if(hash[s[j]]==1) break;
+              int len = j-i+1;
+              maxlen = max(len,maxlen);
+              hash[s[j]] = 1;
+        }
+      }
+       return maxlen;
+    }
+};
