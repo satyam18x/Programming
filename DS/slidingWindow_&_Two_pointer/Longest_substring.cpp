2dbabdf9edd50d3590605f78849c66e313a95591
satyam18x <satyamhaldkar45@gmail.com>
2026-07-06 13:15:54 +0530

    06-07-26

--- DS/slidingWindow_&_Two_pointer/Longest_substring.cpp
+++ DS/slidingWindow_&_Two_pointer/Longest_substring.cpp
@@ -21,3 +21,28 @@ public:
 };
 
 //Tc - O(n) sc-O(256) == O(1)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+      
+      int n=s.size();
+      int l=0,r =0;
+      vector<int> hash(256, -1);
+      int maxlen = 0;
+ 
+
+    while(r < n){
+
+        if(hash[s[r]]!=-1){
+            if(hash[s[r]]>=l){
+                l = hash[s[r]] + 1;
+            }
+        }
+       int len = r-l+1;
+        maxlen = max(len, maxlen);
+        hash[s[r]] = r;
+        r++;
+    }
+    return maxlen;
+    }
+};
