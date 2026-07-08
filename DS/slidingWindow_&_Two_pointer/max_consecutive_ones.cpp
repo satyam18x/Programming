9649786b95b9c6355aaab84bdf9e45ca1f1f62e9
satyam18x <satyamhaldkar45@gmail.com>
2026-07-08 11:50:22 +0530

    08-07-26

--- DS/slidingWindow_&_Two_pointer/max_consecutive_ones.cpp
+++ DS/slidingWindow_&_Two_pointer/max_consecutive_ones.cpp
@@ -1,1 +1,25 @@
 //tc-O(n^2)
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int maxlen = 0;
+
+        for(int i=0;i<n;i++){
+            int zeroes = 0;
+          for(int j=i;j<n;j++)  {
+            
+            if(nums[j]==0)
+              zeroes++;
+
+            if(zeroes <= k){
+                int len = j-i+1;
+                maxlen = max(len,maxlen);
+            }
+            else break; 
+          }
+        }
+        return maxlen;
+    }
+};
