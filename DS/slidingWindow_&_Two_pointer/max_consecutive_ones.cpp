6e2cfa3ff2f455479bee6c85999cb50ffbeae5b2
satyam18x <satyamhaldkar45@gmail.com>
2026-07-08 17:41:03 +0530

    08-07-26

--- DS/slidingWindow_&_Two_pointer/max_consecutive_ones.cpp
+++ DS/slidingWindow_&_Two_pointer/max_consecutive_ones.cpp
@@ -24,3 +24,29 @@ public:
     }
 };
 // sliding window - O(n)
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxlen = 0;
+        int zeroes = 0;
+        int l=0,r=0;
+        
+        while(r<n){
+            if(nums[r]==0) 
+              zeroes++;
+
+            while(zeroes > k) {
+                if(nums[l]==0)
+                zeroes--;
+               l++; 
+            }
+
+           int len=r-l+1;
+             maxlen = max(maxlen,len); 
+             r++;
+            
+        }
+        return maxlen;
+    }
+};
