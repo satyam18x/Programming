8e5316f103226433378325953cb2a34f4ff10011
satyam18x <satyamhaldkar45@gmail.com>
2026-07-16 21:05:26 +0530

    16-07-26

--- DS/slidingWindow_&_Two_pointer/Longest_repeating.cpp
+++ DS/slidingWindow_&_Two_pointer/Longest_repeating.cpp
@@ -1,2 +1,22 @@
 //Longest Repeating character Replacement
 
+class Solution
+public:
+    int characterReplacement(string s, int k) {
+        int freq[26] = {};
+        int left = 0, maxFreq = 0;
+
+        for (int right = 0; right < (int)s.size(); right++) {
+            freq[s[right] - 'A']++;
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+
+            // Window invalid: need more than k replacements
+            if ((right - left + 1) - maxFreq > k) {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+        }
+
+        return (int)s.size() - left;
+    }
+};
