2954bb31660cbcd405e9c50b6edf7afaecfc8903
satyam18x <satyamhaldkar45@gmail.com>
2026-08-29 19:50:36 +0530

    28-08-26

--- OOPS/polymorphism.cpp
+++ OOPS/polymorphism.cpp
@@ -1,1 +1,22 @@
 //run time polymorphism
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal makes sound";
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() override {
+        cout << "Dog barks";
+    }
+};
+
+class Cat : public Animal {
+public:
+    void sound() override {
+        cout << "Cat meows";
+    }
+};
+
