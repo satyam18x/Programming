a55927161adcbd31fe73b88298be6de391678d9e
satyam18x <satyamhaldkar45@gmail.com>
2026-08-29 14:16:37 +0530

    29-08-26

--- OOPS/abstraction.cpp
+++ OOPS/abstraction.cpp
@@ -1,4 +1,30 @@
+// C++ program to demonstrate the use of virtual function
+#include <iostream>
+using namespace std;
 
+class vehicle{
+
+    public :
+     virtual void start(){
+         cout << "Vehicle started" << endl;
+     }
+};
+class
+    car : public vehicle{
+    
+        public :
+        void start(){
+            cout << "Car started" << endl;
+        }
+    };
+
+int main(){
+    car c;
+    c.start();
+    return 0;
+} 
+
+// C++ program to demonstrate the use of abstraction and pure virtual function
 #include <iostream>
 using namespace std;
 
